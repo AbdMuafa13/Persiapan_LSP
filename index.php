a8bfef0e10905452cf6a93a8c7b6ca9ec3101479
AbdMuafa13 <abdmuafa@gmail.com>
2022-08-02 08:57:34 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LSP</title>
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <script src="./assets/js/bootstrap.min.js"></script>
+</head>
+<body>
+    Halaman Index
+    <button class="btn btn-primary">tes</button>
+</body>
+</html>
